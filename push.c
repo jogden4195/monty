25dcebcdc54043e286afa23c7f045829c09edda9
Jenn <jennogden95@gmail.com>
2018-09-26 18:22:27 +0000

    Added isdigit in push and fixed variable names in tokenize

--- push.c
+++ push.c
@@ -7,31 +7,31 @@
  * @line_num: current line count
  * Return: void
  */
-void *push_n(stack_t **head, char **arr, unsigned int line_num)
+void *push_n(char *arr, unsigned int line_num)
 {
 	stack_t *new_node, *temp;
 
-	if (sizeof(*arr) != 2 || !is_num(*arr[1]))
+	if (sizeof(arr) != 2 || !is_num(arr[1]))
 	{
 		fprintf(stderr, "L<%u>: usage: push integer\n", line_num);
 		exit(EXIT_FAILURE);
 	}
-	new_node = (stack_t *)malloc(sizeof(*head) * 8);
+	new_node = malloc(sizeof(stack_t));
 	if (!new_node)
 	{
-		fprintf("Error: malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-	new_node->n = n;
+	new_node->n = atoi(arr[1]);
 	new_node->next = NULL;
-	if (!(*head))
+	if (!(*stack))
 	{
-		new_node->next = *head;
+		new_node->next = *stack;
 		new_node->prev = NULL;
-		*head = new_node;
-		return (new_node);
+		*stack = new_node;
+		return;
 	}
-	temp = *head;
+	temp = *stack;
 	while (temp->next)
 		temp = temp->next;
 	temp->next = new_node;
@@ -53,4 +53,17 @@ int is_num(char **arr)
 			return (0);
 	}
 	return (1);
+}
+
+
+/**
+ * is_digit - checks if character is a digit
+ * @c: character to check
+ * Return: 1 on success, 0 on failure
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
 }
