89118f17e2f8cc2db122ae98a3391cdbdf6f0489
Jenn <jennogden95@gmail.com>
2018-09-26 09:23:06 +0000

    Adding pall.c

---
+++ pall.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * pall - prints all nodes of the stack
+ * @stack: the stack we are printing
+ * @line: the number of the line the instructions are on
+ *
+ * Return: None
+ */
+
+void pall(stack_t **stack, unsigned int line)
+{
+	stack_t *current = *stack;
+
+	if (!stack)
+	{
+		fprintf("L%d: invalid instruction\n", line);
+		exit(EXIT_FAILURE);
+	}
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
+}
