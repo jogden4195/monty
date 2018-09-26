8380e4da10a80e18b378ad23c109d919caebc369
thenicopixie <391@holbertonschool.com>
2018-09-26 02:41:00 +0000

    add error handling. Add function the check is argument is a number or not

--- push.c
+++ push.c
@@ -7,13 +7,21 @@
  * @n: integer value to assign n in the new node
  * Return: the address of the new node, of NULL if it failed
  */
-void *push_n(stack_t **head, unsigned int line_num)
+void *push_n(stack_t **head, char **arr, unsigned int line_num)
 {
 	stack_t *new_node, *temp;
 
+	if (sizeof(*arr) != 2 || !is_num(*arr[1]))
+	{
+		fprintf(stderr, "L<%u>: usage: push integer\n", line_num);
+		exit(EXIT_FAILURE);
+	}
 	new_node = (stack_t *)malloc(sizeof(*head) * 8);
 	if (!new_node)
-		return (NULL);
+	{
+		fprintf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 	new_node->n = n;
 	new_node->next = NULL;
 	if (!(*head))
@@ -28,4 +36,15 @@ void *push_n(stack_t **head, unsigned int line_num)
 		temp = temp->next;
 	temp->next = new_node;
 	new_node->prev = temp;
+}
+int is_num (char **arr)
+{
+	while (*arr)
+	{
+		if (isdigit(*arr))
+			continue;
+		else
+			return (0);
+	}
+	return (1);
 }
