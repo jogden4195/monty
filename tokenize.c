771073dcf31091340b142b4de3c17efe9a03e1c5
thenicopixie <391@holbertonschool.com>
2018-09-26 01:54:05 +0000

    functions that count words and tokanize lines

---
+++ tokenize.c
@@ -0,0 +1,44 @@
+#include "monty.h"
+
+/**
+ * arg_count - number of tokens from getline
+ * @str: string to count number of tokens
+ * Return: number of tokens
+ */
+int arg_count(char *str)
+{
+	int word_count = 0, i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			word_count++;
+		i++;
+	}
+	return (word_count);
+}
+/**
+ * tokenize - function to get tokens from string
+ * and store in an array
+ * @str: string to tokenize
+ * Return: array
+ */
+char **tokenize(char *str)
+{
+	char *token, **arr;
+	int i = 0, words;
+
+	words = arg_count(str);
+	arr = malloc(sizeof(char *) * (words + 1));
+	if (!arr)
+		return (NULL);
+	token = strtok(str, " ");
+	while (token != NULL)
+	{
+		arr[i] = token;
+		token = strtok(NULL, " ");
+		i++;
+	}
+	arr[i] = NULL;
+	return (arr);
+}
