89854bfe21cc08c0c8f3ccee284d9b15d9bb8d64
Jenn <jennogden95@gmail.com>
2018-09-27 17:36:05 +0000

    Adding swap function.

---
+++ swap.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: the stack we are editing
+ * @line: the current line number
+ */
+
+void swap (stack_t **stack, unsigned int line)
+{
+	int i;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%d: can't swap, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+	i = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = i;
+}
