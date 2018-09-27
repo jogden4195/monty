5d1c3ff8c7fc7d7792c25e11673049276aa1fbdd
Jenn <jennogden95@gmail.com>
2018-09-27 19:00:21 +0000

    adding nop function

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * np - fuction thst does nothing
+ * @stack: pointer to head of stack
+ * @line: current line number
+ */
+void nop(stack_t **stack, unsigned int line)
+{
+	(void)*stack;
+	(void)line;
+}
