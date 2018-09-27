9183652d74b7672baf8b3a3772103fc873c4bb21
thenicopixie <391@holbertonschool.com>
2018-09-27 04:32:19 +0000

    update bug fixes. Remove head pointer. refrence stack in function

--- push.c
+++ push.c
@@ -9,12 +9,11 @@
  */
 void push_n(stack_t **stack, unsigned int line_num)
 {
-	stack_t *new_node, *temp, *head;
+	stack_t *new_node, *temp;
 	int n;
-	head = *stack;
 	if (element_counter() != 2)
 	{
-		fprintf(stderr, "L<%u>: usage: push integer\n", line_num);
+		fprintf(stderr, "L%u: usage: push integer\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 	new_node = malloc(sizeof(stack_t));
@@ -26,10 +25,11 @@ void push_n(stack_t **stack, unsigned int line_num)
 	n = atoi(array[1]);
 	new_node->n = n;
 	new_node->prev = NULL;
-	if (head != NULL)
-		(head)->prev = new_node;
-	new_node->next = head;
-	head = new_node;
+	if (*stack != NULL)
+		(*stack)->prev = new_node;
+	new_node->next = *stack;
+	*stack = new_node;
+	return;
 }
 
 /**
