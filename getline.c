5755b872a2aaff3e2d1b1378cea74cfd31eb211d
Jenn <jennogden95@gmail.com>
2018-09-27 00:14:27 +0000

    Fixed bugs in getline

--- getline.c
+++ getline.c
@@ -5,12 +5,13 @@
  * tokenizes the line and then checks if arguments are in a struct of op codes
  * @filename: file to get lines from
  */
-void get_line(const char *filename)
+void get_line(stack_t **stack, const char *filename)
 {
 	ssize_t read;
 	FILE *fptr;
-	char *lineptr = NULL, **args;
+	char *lineptr = NULL;
 	size_t len = 0;
+	int i = 0;
 	unsigned int line_count = 1;
 
 	if (access(filename, F_OK) == -1)
@@ -18,7 +19,7 @@ void get_line(const char *filename)
 		fprintf(stderr, "Error: File <%p> does not exist.\n", filename); 
 		exit(EXIT_FAILURE);
 	}
-	fptr = fopen(filename, O_RDONLY);
+	fptr = fopen(filename, "r");
 	if (!fptr)
 	{
 		fprintf(stderr, "Error: Can't open file <%p>\n", filename);
@@ -26,9 +27,9 @@ void get_line(const char *filename)
 	}
 	while ((read = getline(&lineptr, &len, fptr)) != -1)
 	{
-		args = tokenize(lineptr);
-		get_ops(args, line_count);
-		free(args);
+		array = tokenize(lineptr);
+		get_ops(stack, line_count);
+		free(array);
 		free(lineptr);
 		line_count++;
 	}
