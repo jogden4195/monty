4ceaab39890c41748598339db326553e85d052a1
Jenn <jennogden95@gmail.com>
2018-09-27 18:49:11 +0000

    Fixed betty errors and bug

--- getline.c
+++ getline.c
@@ -3,6 +3,7 @@
 /**
  * get_line - function to get lines from file. Calls a function that
  * tokenizes the line and then checks if arguments are in a struct of op codes
+ * @stack: stack we are editing
  * @filename: file to get lines from
  */
 void get_line(stack_t **stack, const char *filename)
@@ -11,14 +12,8 @@ void get_line(stack_t **stack, const char *filename)
 	FILE *fptr;
 	char *lineptr = NULL;
 	size_t len = 0;
-	int i = 0;
 	unsigned int line_count = 1;
 
-	if (access(filename, F_OK) == -1)
-	{
-		fprintf(stderr, "Error: File %p does not exist.\n", filename); 
-		exit(EXIT_FAILURE);
-	}
 	fptr = fopen(filename, "r");
 	if (!fptr)
 	{
@@ -27,9 +22,8 @@ void get_line(stack_t **stack, const char *filename)
 	}
 	while ((read = getline(&lineptr, &len, fptr)) != -1)
 	{
-		array = tokenize(lineptr);
+		tokenize(lineptr);
 		get_ops(stack, line_count);
-		//free(lineptr);
 		free(array);
 		line_count++;
 	}
