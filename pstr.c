d46eb1bfeebe5b194f319d728e2f14b8948271c9
Jenn <jennogden95@gmail.com>
2018-09-28 21:06:24 +0000

    Adding pstr function.

---
+++ pstr.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+ * pstr - prints the string starting at the top
+ * of the stack, followed by a new line.
+ * @stack: The stack we are going through
+ * @line: The current line number
+ * Return: None
+ */
+void pstr(stack_t **stack, unsigned int line)
+{
+	char c;
+
+	if (*stack == NULL)
+	{
+		printf("\n");
+	}
+	while ((*stack)->next != NULL)
+	{
+		if ((*stack)->n == 0)
+			break;
+		else if ((*stack)->n > 0 && (*stack)->n <= 127)
+		{
+			c = (*stack)->n;
+			printf("%c", c);
+		}
+		else
+		{
+			fprintf(stderr, "L%u: can't pstr, value out of range\n", line);
+			free_stack(stack);
+			free(element_t.lineptr);
+			fclose(element_t.fptr);
+			exit(EXIT_FAILURE);
+		}
+		*stack = (*stack)->next;
+	}
+	printf("\n");
+} 
+
