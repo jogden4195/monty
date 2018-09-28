0bdd28ba9294138f68483c860ffdf904ed352ed9
Jenn <jennogden95@gmail.com>
2018-09-28 17:12:13 +0000

    Adding pchar function

---
+++ pchar.c
@@ -0,0 +1,35 @@
+#include "monty.h"
+
+/**
+ * pchar - prints first char value in stack
+ * @stack: pointer to value stack
+ * @line: current line number
+ */
+void pchar(stack_t **stack, unsigned int line)
+{
+	char c;
+
+	if (*stack)
+	{
+		if ((*stack)->n >= 0 && (*stack)->n <= 127)
+		{
+			c = (*stack)->n;
+			printf("%c\n", c);
+		}
+		else
+		{
+		  fprintf(stderr, "L%u: can't pchar, value out of range", line);
+		  free_stack(stack);
+		  free(element_t.lineptr);
+		  fclose(element_t.fptr);
+		  exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line);
+		free(element_t.lineptr);
+		fclose(element_t.fptr);
+		exit(EXIT_FAILURE);
+	}
+}
