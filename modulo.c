8a924b6fec56b1f3cf6f4fca480c577ddf4651c8
Nicole <40184002+thenicopixie@users.noreply.github.com>
2018-09-28 02:45:34 -0700

    Develop (#60)
    * void line, comment out null check
    
    * changed output to print
    
    * change function prototypes, handle double negative sign
    
    * return exit success
    
    * Update main.c
    
    * function that subtracts two stack values
    
    * add function prototype
    
    * add sub operation
    
    * change order of operation
    
    * fix bugs
    
    * remove unused variable
    
    * add math functions
    
    * fix typo

--- modulo.c
+++ modulo.c
@@ -17,7 +17,9 @@ void modulo(stack_t **stack, unsigned int line_number)
 		fclose(element_t.fptr);
 		exit(EXIT_FAILURE);
 	}
-	if ((*stack)-n == 0)
+
+	if ((*stack)->n == 0)
+
 	{
 		fprintf(stderr, "L%u: division by zero\n", line_number);
 		free_stack(stack);
